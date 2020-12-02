741be68122855e8d03206609e80c904aa05b1b32
Hamid Kianzad <hamid.kianzad@suzohapp.com>
2020-12-02 16:33:49 +0100

    vertx3 event bus

--- src/components/SockJs.tsx
+++ src/components/SockJs.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import EventBus from 'vertx3-eventbus-client';
 // import SockJS from 'sockjs-client';
 
 export interface SockJsProps {
@@ -6,7 +7,19 @@ export interface SockJsProps {
 }
 
 const SockJs: React.FunctionComponent<SockJsProps> = ({ data }) => {
-    console.log(data);
+    const eb = new EventBus('http://localhost:8080/eventbus');
+    eb.onopen = () => {
+        eb.send('chat.to.server', data);
+        eb.registerHandler('chat.to.client', (error: any, message: any) => {
+            if (error) console.log('received error: ' + JSON.stringify(error));
+            else console.log('received a message: ' + JSON.stringify(message));
+        });
+    };
+
+    eb.onclose = () => {
+        console.log('closed');
+    };
+
     // const sock = new SockJS('https://chat-server.azurewebsites.net/chat');
     // sock.onopen = () => {
     //     console.log('connection open');
